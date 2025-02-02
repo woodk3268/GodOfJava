f50cf6ba51d4b526609a30333806a8823bc10068
woodk3268 <155120219+woodk3268@users.noreply.github.com>
2025-02-02 15:56:19 +0900

    11장 deprecated method

---
+++ src/chapter11/APICheck.java
@@ -0,0 +1,13 @@
+package chapter11;
+
+public class APICheck {
+    public static void main(String[] args) {
+        APICheck check = new APICheck();
+        check.useDeprecated();
+    }
+    public void useDeprecated() {
+        String str = "abcde";
+        byte[] strBytes = str.getBytes();
+        String convertedStr = new String(strBytes,0);
+    }
+}
