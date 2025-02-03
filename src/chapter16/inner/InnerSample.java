25caae5954abafbfe6f803c69f7f7cf7f864cd6b
woodk3268 <155120219+woodk3268@users.noreply.github.com>
2025-02-03 22:41:43 +0900

    nested class

---
+++ src/chapter16/inner/InnerSample.java
@@ -0,0 +1,16 @@
+package chapter16.inner;
+
+public class InnerSample {
+
+    public static void main(String[] args) {
+        InnerSample sample = new InnerSample();
+        sample.makeInnerObject();
+    }
+
+    public void makeInnerObject() {
+        OuterOfInner outer = new OuterOfInner();
+        OuterOfInner.Inner inner = outer.new Inner();
+        inner.setValue(3);
+        System.out.println(inner.getValue());
+    }
+}
