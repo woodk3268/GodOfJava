41f52571af374efdc325f6a24043d52c6e2b912a
woodk3268 <155120219+woodk3268@users.noreply.github.com>
2025-02-05 23:19:37 +0900

    annotation override

---
+++ src/chapter17/Parent.java
@@ -0,0 +1,11 @@
+package chapter17;
+
+public class Parent {
+    public Parent() {
+        System.out.println("Parent Constructor");
+    }
+
+    public void printName() {
+        System.out.println("Parent printName()");
+    }
+}
